823c5cc54a05ef8e30fb86d0e8c8a056451b2fe7
Eugene <ernestyj@outlook.com>
2016-03-29 14:49:37 +0800

    CassandraTest

--- build.sbt
+++ build.sbt
@@ -12,3 +12,13 @@ lazy val root = (project in file(".")).
             libraryDependencies ++= rootDependencies
         )
 
+assemblyMergeStrategy in assembly := {
+    case _ => MergeStrategy.deduplicate
+//    case PathList("javax", "servlet", xs @ _*)         => MergeStrategy.first
+//    case PathList(ps @ _*) if ps.last endsWith ".html" => MergeStrategy.first
+//    case "application.conf"                            => MergeStrategy.concat
+//    case "unwanted.txt"                                => MergeStrategy.discard
+//    case x =>
+//        val oldStrategy = (assemblyMergeStrategy in assembly).value
+//        oldStrategy(x)
+}
--- src/main/scala/CassandraTest.scala
+++ src/main/scala/CassandraTest.scala
@@ -1,6 +1,8 @@
+import java.util.Date
+
 import com.datastax.driver.core.ResultSet
 import com.datastax.spark.connector.cql.CassandraConnector
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
 
 /**
@@ -11,20 +13,19 @@ object CassandraTest {
         val conf = new SparkConf(true).
 //            set("spark.cassandra.connection.host", "192.168.1.106").
 //            set("spark.cassandra.connection.host", "202.120.40.111")
-            set("spark.cassandra.connection.host", "121.41.126.207")
+//            set("spark.cassandra.connection.host", "121.41.126.207")
+            set("spark.cassandra.connection.host", "10.253.26.54")
         val sc = new SparkContext("local", "test", conf)
 
-        val conn = CassandraConnector(conf)
-        conn.withSessionDo{ session =>
-//            var result = session.execute("select * from stock_market.stock_day_open")
-            var result = session.execute("select * from test.kv")
-            println(result.one())
-        }
-//        val rdd = sc.cassandraTable("stock_market", "stock_day_open")
-//        val rdd = sc.cassandraTable("test", "kv")
-//        println(rdd.count)
-//        println(rdd.first)
-//        println(rdd.map(_.getInt("value")).sum)
+//        val conn = CassandraConnector(conf)
+//        conn.withSessionDo{ session =>
+////            var result = session.execute("select * from stock_market.stock_day_open")
+//            var result = session.execute("select * from market_quotation.stock_day_open")
+//            println(result.one())
+//        }
+        val tableRDD = sc.cassandraTable[CassandraRow]("market_quotation", "stock_day_open")
+        val stock = tableRDD.where("stock=?", "000001.SZ")
+        stock.foreach(col => println(col.get[String]("day").toString+" "+col.get[Float]("open")))
 
 //        val collection = sc.parallelize(Seq(("key3", 3), ("key4", 4)))
 //        collection.saveToCassandra("test", "kv", SomeColumns("key", "value"))
